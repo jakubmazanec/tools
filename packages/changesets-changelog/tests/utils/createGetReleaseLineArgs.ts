jakubmazanec/js-tools

Create @jakubmazanec/changesets-changelog

packages/changesets-changelog/tests/utils/createGetReleaseLineArgs.ts
@@ -1,0 +1,25 @@
+import parse from '@changesets/parse';
+
+export function createGetReleaseLineArgs(
+  content: string,
+  commit: string | undefined,
+  repo: string,
+) {
+  return [
+    {
+      ...parse(
+        `---
+  pkg: "minor"
+  ---
+
+  something
+  ${content}
+  `,
+      ),
+      id: 'some-id',
+      commit,
+    },
+    'minor',
+    {repo},
+  ] as const;
+}

jakubmazanec/tools

packages/changesets-changelog/tests/utils/createGetReleaseLineArgs.ts
@@ -1,0 +1,25 @@
+import parse from '@changesets/parse';
+
+export function createGetReleaseLineArgs(
+  content: string,
+  commit: string | undefined,
+  repository: string,
+) {
+  return [
+    {
+      ...parse(
+        `---
+  pkg: "minor"
+  ---
+
+  something
+  ${content}
+  `,
+      ),
+      id: 'some-id',
+      commit,
+    },
+    'minor',
+    {repository},
+  ] as const;
+}