650276dfc8ff6f3d408dea033ef385b275d5de56
Jakub Mazanec <jakub@mazanec.dev>
2024-01-30 21:24:40 +0100

    Update Carson templates

--- packages/carson-templates/source/getWorkspaceDirectoryName.ts
+++
@@ -1,6 +0,0 @@
-import {type Workspace} from '@jakubmazanec/carson';
-import path from 'node:path';
-
-export function getWorkspaceDirectoryName(workspace: Workspace | Workspace<false>) {
-  return path.basename(workspace.path);
-}
