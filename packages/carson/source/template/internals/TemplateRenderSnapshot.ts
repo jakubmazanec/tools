5549c507305841552b268bdbd841b29630bea1cb
Jakub Mazanec <jakub@mazanec.dev>
2025-01-05 22:28:29 +0100

    Fix @jakubmazanec/carson

--- packages/carson/source/template/internals/TemplateRenderSnapshot.ts
+++ packages/carson/source/template/internals/TemplateRenderSnapshot.ts
@@ -3,11 +3,9 @@ import {z} from 'zod';
 
 import {templateAttributesSchema} from '../TemplateAttributes.js';
 
-export const templateRenderSnapshotSchema = z
-  .object({
-    attributes: baseTemplateAttributesSchema.pick({to: true}).merge(templateAttributesSchema),
-    content: z.string().optional(),
-  })
-  .strict();
+export const templateRenderSnapshotSchema = z.object({
+  attributes: baseTemplateAttributesSchema.pick({to: true}).merge(templateAttributesSchema),
+  content: z.string().optional(),
+});
 
 export type TemplateRenderSnapshot = z.infer<typeof templateRenderSnapshotSchema>;
