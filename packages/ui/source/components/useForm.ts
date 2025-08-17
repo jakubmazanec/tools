f4170eaca041f87e513ea348f654008deb811765
Jakub Mazanec <jakub@mazanec.dev>
2025-08-17 11:31:56 +0200

    Update @jakubmazanec/ui

--- packages/ui/source/components/useForm.ts
+++ packages/ui/source/components/useForm.ts
@@ -1,24 +1,35 @@
 import {useForm as useBaseForm} from '@conform-to/react';
-import {parseWithZod} from '@conform-to/zod';
-import {type ZodTypeAny} from 'zod';
+import {parseWithZod} from '@conform-to/zod/v4';
+import {type output, type ZodObject} from 'zod';
 
-export type UseFormOptions<S extends ZodTypeAny> = {
+export type UseFormOptions<S extends ZodObject> = {
   schema: S;
   onSubmit?: Parameters<typeof useBaseForm<S>>[0]['onSubmit'] | undefined;
 };
 
-export function useForm<S extends ZodTypeAny>({schema, onSubmit}: UseFormOptions<S>) {
+export type UseFormReturn<S extends ZodObject> = {
+  form: ReturnType<typeof useBaseForm<output<S>>>[0];
+  fields: ReturnType<typeof useBaseForm<output<S>>>[1];
+};
+
+export function useForm<S extends ZodObject>({
+  schema,
+  onSubmit,
+}: UseFormOptions<S>): UseFormReturn<S> {
   let [form, fields] = useBaseForm({
     shouldValidate: 'onBlur',
     shouldRevalidate: 'onInput',
+    // TODO: fix this
+    // @ts-expect-error -- fix this
     onValidate({formData}) {
       return parseWithZod(formData, {schema});
     },
-    onSubmit: onSubmit as NonNullable<typeof onSubmit>, // TODO: remove when this is fixed: https://github.com/edmundhung/conform/issues/697
+    onSubmit,
   });
 
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions -- false positive
   return {
     form,
     fields,
-  };
+  } as UseFormReturn<S>;
 }
