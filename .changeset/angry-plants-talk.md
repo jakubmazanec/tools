2fd6531fd2bb8646a07b03f74cfe3d3e55d7e866
Jakub Mazanec <jakub@mazanec.dev>
2026-02-08 17:30:26 +0100

    Fix @jakubmazanec/ui

---
+++ .changeset/angry-plants-talk.md
@@ -0,0 +1,5 @@
+---
+'@jakubmazanec/ui': patch
+---
+
+Fix `DataTable` search functionality.
--- packages/ui/source/components/data-table/internals/fuzzyFilter.ts
+++ packages/ui/source/components/data-table/internals/fuzzyFilter.ts
@@ -9,6 +9,18 @@ export function fuzzyFilter<TData extends RowData>(
 ) {
   let itemRank = rankItem(row.getValue(columnId), value as string, {});
 
+  // @ts-expect-error - DEBUG
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access -- DEBUG
+  if (row.original.name?.toLowerCase().includes('ardbeg')) {
+    console.log(columnId, 'row', row, 'itemRank', itemRank);
+
+    // @ts-expect-error - DEBUG
+    if (row.original.order === 2358) {
+      // eslint-disable-next-line no-debugger -- DEBUG
+      debugger;
+    }
+  }
+
   addMeta({itemRank});
 
   return itemRank.passed;
